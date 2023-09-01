b53e79fe10761f4f540da4d8bf63cc54af483147
Caruy Chen <caruychen@gmail.com>
2023-09-01 14:24:59 +0300

    define sizes

--- includes/malloc.h
+++ includes/malloc.h
@@ -6,6 +6,11 @@
 # include <unistd.h>
 # include <pthread.h>
 
+# define SIZE_TINY_HEAP (getpagesize())
+# define SIZE_SMALL_HEAP (getpagesize() * 0x20)
+# define SIZE_TINY_BLOCK (SIZE_TINY_HEAP / 0x80)
+# define SIZE_SMALL_BLOCK (SIZE_SMALL_HEAP / 0x80)
+
 void	*malloc(size_t size);
 
 #endif
